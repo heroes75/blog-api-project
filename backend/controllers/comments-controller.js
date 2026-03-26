148886165b7138e8477e4802747ab30f2a24676a
heroes75 <123456789+odin@users.noreply.github.com>
2026-03-26 04:41:21 +0000

    finish th backend party

---
+++ backend/controllers/comments-controller.js
@@ -0,0 +1,100 @@
+const { prisma } = require("../lib/prisma");
+
+const postNoFound = "no post found in the system"
+
+async function postComment(req, res) {
+    const {postId} = req.params;
+    const {text} = req.body
+    const post = await prisma.posts.findUnique({
+        where: {
+            id: postId
+        }
+        
+    })
+    if (!post) {
+        res.status(404).json({
+            message: postNoFound
+        })
+    }
+
+    const comment = await prisma.comments.create({
+        data: {
+            text,
+            authorId: req.user.id,
+            postsId: post.id
+        }
+    })
+
+    res.status(200).json({
+        comment
+    })
+}
+
+async function updateComment(req, res) {
+    const {postId, commentId} = req.params
+    const {text} = req.body
+    const post = await prisma.posts.findUnique({
+        where: {
+            id: postId
+        }
+    })
+    const comment = await prisma.comments.findUnique({
+        where: {
+            id: commentId
+        }
+    })
+
+    if (!post || !comment) {
+        res.status(404).json({
+            message: `no ${!post ? "post" : "comment"} found in the system`
+        })
+    }
+
+    const updatedComment = await prisma.comments.update({
+        where: {
+            id: commentId
+        },
+        data: {
+            text
+        }
+    })
+
+    res.status(200).json({
+        updatedComment
+    })
+}
+
+async function deleteComment(req, res) {
+    const {postId, commentId} = req.params
+    const post = await prisma.posts.findUnique({
+        where: {
+            id: postId
+        }
+    })
+    const comment = await prisma.comments.findUnique({
+        where: {
+            id: commentId
+        }
+    })
+
+    if (!post || !comment) {
+        res.status(404).json({
+            message: `no ${!post ? "post" : "comment"} found in the system`
+        })
+    }
+
+    const deletedComment = await prisma.comments.delete({
+        where: {
+            id: commentId
+        }
+    })
+
+    res.status(200).json({
+        deletedComment
+    })
+}
+module.exports = {
+    postComment,
+    updateComment,
+    deleteComment
+}
--- backend/routes/posts-router.js
+++ backend/routes/posts-router.js
@@ -2,6 +2,7 @@
 const {Router} = require('express')
 const passport = require('passport')
 const { getAllPosts, createPost, updatePost, deletePost, getAllPostOfUser, getPost } = require('../controllers/posts-controller')
+const { postComment, updateComment, deleteComment } = require('../controllers/comments-controller')
 // const strategy = require('../config/passport')
 // passport.use(strategy)
 
@@ -11,7 +12,10 @@ postsRouter.get('/', passport.authenticate('jwt', {session: false}), getAllPosts
 postsRouter.post('/', passport.authenticate('jwt', {session: false}), createPost)
 postsRouter.get('/dashboard', passport.authenticate('jwt', {session: false}), getAllPostOfUser)
 postsRouter.get('/:postId', passport.authenticate('jwt', {session: false}), getPost)
+postsRouter.post('/:postId/comments', passport.authenticate('jwt', {session: false}), postComment)
 postsRouter.put('/:postId', passport.authenticate('jwt', {session: false}), updatePost)
 postsRouter.delete('/:postId', passport.authenticate('jwt', {session: false}), deletePost)
+postsRouter.put('/:postId/comments/:commentId', passport.authenticate('jwt', {session: false}), updateComment)
+postsRouter.delete('/:postId/comments/:commentId', passport.authenticate('jwt', {session: false}), deleteComment)
 
 module.exports = postsRouter

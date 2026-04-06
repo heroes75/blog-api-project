fb6189ad2e0b6847e26731f86075e6786fb5fc74
heroes75 <123456789+odin@users.noreply.github.com>
2026-04-06 18:47:25 +0000

    try to make a constraint validation with react

--- frontend-authors/src/Components/signupPage.jsx
+++ frontend-authors/src/Components/signupPage.jsx
@@ -1,9 +1,21 @@
+import { useRef, useState } from "react"
+
 export default function SignupForm() {
+    const [username, setUsername] = useState('')
+    const [passwrd, setPasswrd] = useState('')
+    const [confirmPasswrd, setConfirmPasswrd] = useState('')
+    const inputUsername = useRef(null)
+    const inputPassword = useRef(null)
+    const inputConfirmPassword = useRef(null)
+    function handleUsername (e) {
+        setUsername(e.target.value)
+    }
     return (
         <>
             <form role="form" action="" method="post">
+                <ul data-testid="message-error"></ul>
                 <label htmlFor="username">Enter your username: </label>
-                <input type="text" id="username" name="username" placeholder="your username" />
+                <input value={username} onChange={(e) => handleUsername(e)} ref={inputUsername}  type="text" id="username" name="username" placeholder="your username" />
                 <label htmlFor="password">Enter your password: </label>
                 <input type="password" id="password" name="password" placeholder="your password" />
                 <label htmlFor="confirmPassword">Confirm your password: </label>
--- frontend-authors/src/tests/signupPage.test.jsx
+++ frontend-authors/src/tests/signupPage.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { describe, test, vi, expect, beforeEach } from "vitest";
 import SignupForm from "../Components/signupPage";
+import userEvent from "@testing-library/user-event";
 
 describe("should be have an input and label username, an input and label password and confirm password and a button and a form", () => {
     beforeEach(() => {
@@ -24,4 +25,26 @@ describe("should be have an input and label username, an input and label passwor
         expect(form).toContainElement(labelConfirmPassword);
         expect(form).toContainElement(button);
     });
+
+    
 });
+
+describe('if user wrong input it\'s should be show the right message error', () => {
+    beforeEach(() => {
+        render(<SignupForm />)
+    })
+    test('if user type empty it\'s should be show "Please enter.."', async () => {
+        const inputUsername = screen.getByPlaceholderText('your username')
+        const inputPassword = screen.getByPlaceholderText('your password')
+        const inputConfirmPassword = screen.getByPlaceholderText('confirm your password')
+        const button = screen.getByRole('button');
+        const messageError = screen.getByTestId('message-error');
+        const user = userEvent.setup()
+        // await user.type(inputUsername, 'qw')
+        // await user.type(inputPassword, '')
+        // await user.type(inputConfirmPassword, '')
+        await user.click(button)
+        expect(messageError).toContainHTML('<li>Please enter an username</li>')
+        expect(messageError).toContainHTML('<li>Please enter an password</li>')
+    })
+})

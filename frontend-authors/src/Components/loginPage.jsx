ab75d31cdc2b7e9b065021a9c7b7010736a85eb4
heroes75 <123456789+odin@users.noreply.github.com>
2026-04-09 18:29:19 +0000

    implementing login function

--- frontend-authors/src/Components/loginPage.jsx
+++ frontend-authors/src/Components/loginPage.jsx
@@ -1,11 +1,45 @@
+import { useState } from "react"
+import { useNavigate } from "react-router"
+
 export default function Login() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [messageError, setMessageError] = useState('')
+    const navigate = useNavigate()
+
+    function handleUsername(e) {
+        setUsername(e.target.value)
+    }
+
+    function handlePassword(e) {
+        setPassword(e.target.value)
+    }
+
+    function handleSubmit() {
+        fetch('http://localhost:8000/login', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({username, password})
+        }).then(res => res.json()).then(res => {
+            console.log('res:', res)
+            if (res.statusCode >= 400) {
+                return setMessageError(res.message)
+            }  
+        })
+        navigate('/home')
+    }
     return (
         <>
+        <ul>
+            {messageError && <li>{messageError}</li>}
+        </ul>
             <label htmlFor="username">Enter your username:</label>
-            <input type="text" name="username" id="username" placeholder="your username" />
+            <input value={username} onChange={handleUsername} type="text" name="username" id="username" placeholder="your username" />
             <label htmlFor="password">Enter your password:</label>
-            <input type="password" name="password" id="password" placeholder="your password"/>
-            <button type="submit">Submit</button>
+            <input value={password} onChange={handlePassword} type="password" name="password" id="password" placeholder="your password"/>
+            <button onClick={handleSubmit} type="submit">Submit</button>
         </>
     )
 }
--- frontend-authors/src/tests/loginPage.test.jsx
+++ frontend-authors/src/tests/loginPage.test.jsx
@@ -2,25 +2,97 @@ import { vi, describe, test, expect, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { createMemoryRouter, RouterProvider } from "react-router";
 import routes from "../routes";
-import Login from "../Components/loginPage";
+import userEvent from "@testing-library/user-event";
 
+const navigate = vi.fn(() => {});
+vi.mock(import("react-router"), async (importOriginal) => {
+    const mod = await importOriginal()
+    return {
+        ...mod,
+        useNavigate: () => navigate,
+    };
+});
 beforeEach(() => {
-    const router = createMemoryRouter(routes, {initialEntries: ['/login']})
-    render(<RouterProvider router={router}/>)
-})
-describe('test the presence of components', () => {
-    test('it\'s should be have a input Component, password component and a button', () => {
-        const inputUsername = screen.getByPlaceholderText('your username')
-        const labelUsername = screen.getByLabelText('Enter your username:')
-        const labelPassword = screen.getByLabelText('Enter your password:')
-        const inputPassword = screen.getByPlaceholderText('your password')
-        const button = screen.getByRole('button')
-        expect(inputPassword).toBeInTheDocument()
-        expect(inputUsername).toBeInTheDocument()
-        expect(labelUsername).toBeInTheDocument()
-        expect(labelPassword).toBeInTheDocument()
-        expect(button).toBeInTheDocument()
-    })
-})
+    const router = createMemoryRouter(routes, { initialEntries: ["/login"] });
+    render(<RouterProvider router={router} />);
+});
+describe("test the presence of components", () => {
+    test("it's should be have a input Component, password component and a button", () => {
+        const inputUsername = screen.getByPlaceholderText("your username");
+        const labelUsername = screen.getByLabelText("Enter your username:");
+        const labelPassword = screen.getByLabelText("Enter your password:");
+        const inputPassword = screen.getByPlaceholderText("your password");
+        const button = screen.getByRole("button");
+        expect(inputPassword).toBeInTheDocument();
+        expect(inputUsername).toBeInTheDocument();
+        expect(labelUsername).toBeInTheDocument();
+        expect(labelPassword).toBeInTheDocument();
+        expect(button).toBeInTheDocument();
+    });
+});
 
-describe()
+describe("if user type wrong input it's should be display the message error", () => {
+    test("if user type an incorrect username it's should display on message error", async () => {
+        window.fetch = vi.fn(() =>
+            Promise.resolve({
+                json: () => ({
+                    message: "incorrect username",
+                    statusCode: 401,
+                }),
+            }),
+        );
+        const inputUsername = screen.getByPlaceholderText("your username");
+        const inputPassword = screen.getByPlaceholderText("your password");
+        const button = screen.getByRole("button");
+        const user = userEvent.setup();
+        await user.type(inputUsername, "qwerty");
+        await user.type(inputPassword, "qwerty");
+        await user.click(button);
+        expect(screen.getAllByRole("listitem").length).toEqual(1);
+        expect(screen.getAllByRole("listitem")[0].textContent).toBe(
+            "incorrect username",
+        );
+    });
+    test("if user type an incorrect it's should display an error", async () => {
+        window.fetch = vi.fn(() =>
+            Promise.resolve({
+                json: () => ({
+                    message: "incorrect password",
+                    statusCode: 401,
+                }),
+            }),
+        );
+        const inputUsername = screen.getByPlaceholderText("your username");
+        const inputPassword = screen.getByPlaceholderText("your password");
+        const button = screen.getByRole("button");
+        const user = userEvent.setup();
+        await user.type(inputUsername, "qwerty");
+        await user.type(inputPassword, "qwerty");
+        await user.click(button);
+        expect(screen.getAllByRole("listitem").length).toEqual(1);
+        expect(screen.getAllByRole("listitem")[0].textContent).toBe(
+            "incorrect password",
+        );
+    });
+});
+
+describe("if user type the right username and password it's should connected", () => {
+    test("test connection", async () => {
+        window.fetch = vi.fn(() =>
+            Promise.resolve({
+                json: () => ({
+                    user: { username: "", password: "" },
+                    token: "token",
+                }),
+            }),
+        );
+        const inputUsername = screen.getByPlaceholderText("your username");
+        const inputPassword = screen.getByPlaceholderText("your password");
+        const button = screen.getByRole("button");
+        const user = userEvent.setup();
+        await user.type(inputUsername, "qwerty");
+        await user.type(inputPassword, "qwerty");
+        await user.click(button);
+        expect(navigate).toHaveBeenCalledExactlyOnceWith("/home");
+    });
+});
